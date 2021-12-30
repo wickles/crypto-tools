a26209519f054af27d96e718ce6454302012c234
wickles <wickles137@gmail.com>
2021-12-29 23:24:53 -0800

    Fix incorrect gas cost from etherscan queries

--- cointracker-etherscan-export.py
+++ cointracker-etherscan-export.py
@@ -12,44 +12,42 @@ address = config['evm']['address']
 url_base = config['evm']['scan_url']
 gas_token = config['evm']['gas_token']
 gas_decimal = int(config['evm']['gas_decimal'])
+# TODO: take API key in credentials file
 api_key = "YourApiKeyToken"
 
 # urls for requests
-url_normal="{}/api?module=account&action=txlist&address={}&sort=asc&apikey={}".format(url_base, address, api_key)
 url_erc20="{}/api?module=account&action=tokentx&address={}&sort=asc&apikey={}".format(url_base, address, api_key)
+
+# Build URL for receipt of a given tx hash
+def txReceiptUrl(hash):
+    return "{}/api?module=proxy&action=eth_getTransactionReceipt&txhash={}&apikey={}".format(url_base, hash, api_key)
 
 csvfile = open('trades.csv', 'w', newline='')
 writer = csv.writer(csvfile)
 writer.writerow(['Date', 'Received Quantity', 'Received Currency', 'Sent Quantity', 'Sent Currency', 'Fee Amount', 'Fee Currency', 'Tag'])
 
-# normal transactions
-response = requests.get(url_normal)
-for item in response.json()["result"]:
-    # Relevant keys: timeStamp, from, to, value, tokenDecimal, tokenSymbol, gasPrice, gasUsed, input
-    if (int(item['value']) == 0): continue
-    decimals = gas_decimal
-    timestamp = datetime.datetime.utcfromtimestamp(int(item['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
-    value_proper = int(item['value']) * pow(10, -decimals)
-    gas_proper = int(item['gasUsed']) * int(item['gasPrice']) * pow(10, -decimals) * 0.5
-    symbol = gas_token
-    dir = 'in ' if item['to'].casefold() == address.casefold() else 'out'
-    print("{}: {}, {}, {}, {}, {}".format(dir, timestamp, value_proper, symbol, gas_proper, gas_token))
-
-
 # token transactions
 response = requests.get(url_erc20)
-for item in response.json()["result"]:
-    # Relevant keys: timeStamp, from, to, value, tokenDecimal, tokenSymbol, gasPrice, gasUsed
+results = response.json()["result"]
+index = 0
+for item in results:
+    index += 1
+
+    # Collect relevant values
     decimals = int(item['tokenDecimal'])
     timestamp = datetime.datetime.utcfromtimestamp(int(item['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
     value_proper = int(item['value']) * pow(10, -decimals)
-    gas_proper = int(item['gasUsed']) * int(item['gasPrice']) * pow(10, -decimals) * 0.5
     symbol = item['tokenSymbol']
 
-    # dir = 'in ' if item['to'].casefold() == address.casefold() else 'out'
-    # print("{}: {}, {}, {}, {}, {}".format(dir, timestamp, value_proper, symbol, gas_proper, gas_token))
+    # Query transaction receipt for effective gas price
+    print("Query ({} / {}): {}".format(index, len(results), item['hash']))
+    url_tx = txReceiptUrl(item['hash'])
+    resp_tx = requests.get(url_tx)
+    item_tx = resp_tx.json()["result"]
+    gas_proper = int(item['gasUsed']) * int(item_tx['effectiveGasPrice'],0) * pow(10, -decimals)
 
+    # Write line to file
     if item['to'].casefold() == address.casefold(): #received
         writer.writerow([timestamp, value_proper, symbol, '', '', gas_proper, gas_token, ''])
-    # else: # sent
-    #     writer.writerow([timestamp, '', '', value_proper, symbol, gas_proper, gas_token, ''])
+    else: # sent
+        writer.writerow([timestamp, '', '', value_proper, symbol, gas_proper, gas_token, ''])
