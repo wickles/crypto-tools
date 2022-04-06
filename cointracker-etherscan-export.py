738b5904b2308232a43695817e3b9fd08f8fff88
wickles <wickles137@gmail.com>
2022-04-05 23:05:30 -0700

    Etherscan export for 'normal' transactions
    Export normal and token transactions separately. Includes timeout and
    retry to work around API rate limiting. Includes edge case handling for
    Arbitrum L2 deposits.

--- cointracker-etherscan-export.py
+++ cointracker-etherscan-export.py
@@ -1,3 +1,4 @@
+from typing import Text
 import requests
 import json
 import csv
@@ -13,17 +14,63 @@ api_key = config['evm']['api_key']
 url_base = config['evm']['scan_url']
 gas_token = config['evm']['gas_token']
 gas_decimal = int(config['evm']['gas_decimal'])
+# edge cases
+edge_arb_dom = 'api.arbiscan.io'
+edge_arb_mint = '0x000000000000000000000000000000000000006E'
 
 # urls for requests
+url_normal="{}/api?module=account&action=txlist&address={}&sort=asc&apikey={}".format(url_base, address, api_key)
 url_erc20="{}/api?module=account&action=tokentx&address={}&sort=asc&apikey={}".format(url_base, address, api_key)
 
 # Build URL for receipt of a given tx hash
 def txReceiptUrl(hash):
     return "{}/api?module=proxy&action=eth_getTransactionReceipt&txhash={}&apikey={}".format(url_base, hash, api_key)
 
+# Write Header
 csvfile = open('trades.csv', 'w', newline='')
 writer = csv.writer(csvfile)
 writer.writerow(['Date', 'Received Quantity', 'Received Currency', 'Sent Quantity', 'Sent Currency', 'Fee Amount', 'Fee Currency', 'Tag'])
+
+# normal transactions
+response = requests.get(url_normal)
+results = response.json()["result"]
+index = 0
+for item in results:
+    index += 1
+
+    # Relevant keys: timeStamp, from, to, value, tokenDecimal, tokenSymbol, gasPrice, gasUsed, input
+    #if (int(item['value']) == 0): continue
+    #decimals = gas_decimal
+    timestamp = datetime.datetime.utcfromtimestamp(int(item['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
+    value_proper = int(item['value']) * pow(10, -gas_decimal)
+    symbol = gas_token
+
+    edge_arb_condition = item['from'].casefold() == address.casefold() and \
+                         item['to'].casefold() == edge_arb_mint.casefold() and \
+                         url_base.casefold().find(edge_arb_dom) != -1
+    if item['to'].casefold() == address.casefold() or edge_arb_condition:
+        # received, no gas cost
+        gas_proper = '0'
+        # Write line to file
+        writer.writerow([timestamp, value_proper, symbol, '', '', gas_proper, gas_token, ''])
+    else: # sent, need to get gas cost
+        # Query transaction receipt for effective gas price
+        print("Query ({} / {}): {}".format(index, len(results), item['hash']))
+        for attempts in range(5):
+            try:
+                url_tx = txReceiptUrl(item['hash'])
+                resp_tx = requests.get(url_tx)
+                item_tx = resp_tx.json()["result"]
+                gas_proper = int(item['gasUsed']) * int(item_tx['effectiveGasPrice'],0) * pow(10, -gas_decimal)
+            except:
+                print("Error occurred while handling query...")
+            else:
+                break
+        else:
+            print("Timedout handling that query!")
+
+        # Write line to file
+        writer.writerow([timestamp, '', '', value_proper, symbol, gas_proper, gas_token, ''])
 
 # token transactions
 response = requests.get(url_erc20)
@@ -33,9 +80,9 @@ for item in results:
     index += 1
 
     # Collect relevant values
-    decimals = int(item['tokenDecimal'])
+    token_decimal = int(item['tokenDecimal'])
     timestamp = datetime.datetime.utcfromtimestamp(int(item['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
-    value_proper = int(item['value']) * pow(10, -decimals)
+    value_proper = int(item['value']) * pow(10, -token_decimal)
     symbol = item['tokenSymbol']
 
     # Query transaction receipt for effective gas price
@@ -43,7 +90,7 @@ for item in results:
     url_tx = txReceiptUrl(item['hash'])
     resp_tx = requests.get(url_tx)
     item_tx = resp_tx.json()["result"]
-    gas_proper = int(item['gasUsed']) * int(item_tx['effectiveGasPrice'],0) * pow(10, -decimals)
+    gas_proper = int(item['gasUsed']) * int(item_tx['effectiveGasPrice'],0) * pow(10, -gas_decimal)
 
     # Write line to file
     if item['to'].casefold() == address.casefold(): #received
